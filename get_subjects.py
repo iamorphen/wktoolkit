d76b3955efaebf734ce2070ef35daf9243105142
orphen <orphen@orphen.me>
2019-10-22 11:11:45 -0700

    subjects: add to_anki for exporting subject to Anki
    This required a refactor and is bundled with various touch-ups and
    fixes.
    wktoolkit was renamed to get_subjects. Args handling in get_subjects
    is now scoped into its own function. A mutually-exclusive group of
    arguments is added to support printing original JSON, an Anki format,
    or the Anki schema. Members in Subjects and derived classes are
    reordered as their order of definition is the Anki schema. A
    Subject's aux_meaning is fixed to no longer incorrectly report the
    primary meaning.
    to_anki() is not fully implemented for Kanji and Vocabulary.

--- get_subjects.py
+++ get_subjects.py
@@ -7,10 +7,14 @@ from itertools import chain
 
 from identity import identity
 from interface.interface import Interface
+from interface.subjects import Radical, Kanji, Vocabulary
 from session.session import Session, BASE_URL
 
 
-def main():
+def handle_args():
+  """
+  @return the object returned by argparse.ArgumentParser.parse_args().
+  """
   parser = argparse.ArgumentParser(description=(
     'Fetch WaniKani subjects. Specify no subject type to fetch all types.'))
   parser.add_argument('--token', help='store/use this token for transactions')
@@ -21,36 +25,49 @@ def main():
   parser.add_argument('--level', help=('fetch subjects for this level [1-60]; '
                       'leave unspecified to fetch subjects for all levels'),
                       type=int, default=0)
-  parser.add_argument('--json', help=('print json instead of reified '
-                      'representation to stdout'), action='store_true')
+  group = parser.add_mutually_exclusive_group()
+  group.add_argument('--original_json', help=('print original subject JSON to '
+                     'stdout'), action='store_true')
+  group.add_argument('--anki', help=('print a tab-separated list of fields '
+                     'per subject per line'), action='store_true')
+  group.add_argument('--anki_schema', help='print the Anki schema',
+                     action='store_true')
   parser.add_argument('user', help='the WaniKani username to transact as')
-  args = parser.parse_args()
+  return parser.parse_args()
+
+
+def main():
+  args = handle_args()
+
+  if args.anki_schema:
+    print('Radicals: {}\nKanji: {}\nVocabulary: {}'.format(
+      Radical.anki_schema(), Kanji.anki_schema(), Vocabulary.anki_schema()))
+    sys.exit(0)
 
   token = identity.handle_identity(args.user, args.token)
-
   session = Session(token)
   if not session:
     sys.exit(1)
 
   interface = Interface(session, BASE_URL)
   subjects = interface.get_subjects(args.radical, args.kanji, args.vocabulary,
-                                    args.level, args.json)
+                                    args.level, args.original_json)
 
-  if (args.json):
+  if args.original_json:
     # NOTE(orphen) This could be optimized if creating the single massive JSON
     # object can be avoided.
     jlob = list()
     for item in chain(subjects.radicals, subjects.kanji, subjects.vocabulary):
-      jlob.append(item.json)
+      jlob.append(item.original_json)
     for chunk in json.JSONEncoder().iterencode(jlob):
       print(chunk)
+  elif args.anki:
+    for item in chain(subjects.radicals, subjects.kanji, subjects.vocabulary):
+      print(item.to_anki())
   else:
-    for radical in subjects.radicals:
-      print(radical)
-    for kanji in subjects.kanji:
-      print(kanji)
-    for vocabulary in subjects.vocabulary:
-      print(vocabulary)
+    for item in chain(subjects.radicals, subjects.kanji, subjects.vocabulary):
+      print(item)
+
 
 if __name__ == "__main__":
   main()
--- interface/subjects.py
+++ interface/subjects.py
@@ -6,36 +6,66 @@ vocabulary.
 
 from collections import namedtuple
 
-# radicals, kanji, and vocabulary shall be lists of the following classes.
+# Radicals, kanji, and vocabulary shall be lists of the following classes.
 Subjects = namedtuple('Subjects', ['radicals', 'kanji', 'vocabulary'])
 
 
 class Subject:
+  """
+  A base class for WaniKani subjects.
+  """
+
+  # Use a tab to separate fields as it's far more likely WaniKani will use
+  # commas or semicolons in their content than tabs.
+  ANKI_SEPARATOR = '\t'
+
   def __init__(self, item, store_json):
     """
-    A base class for subjects.
-
     @p item (dict) A dictionary of the JSON subject object retrieved through
       the WaniKani V2 API.
     @p store_json (bool) If true, store @p item in this instance.
     """
-    self.json = item if store_json else None
+    # The definition of members is important. We take the order of definition
+    # to be the order in which our members are written out in the Anki schema.
 
     data = item['data']
 
+    self.original_json = item if store_json else None  # Schema-ignored.
     self.id = item['id']
     self.document_url = data['document_url']
     self.level = data['level']
-
     self.meanings = []
+    self.aux_meanings = []
+    self.meaning_mnemonic = data['meaning_mnemonic']
+
     for meaning in data['meanings']:
       self.meanings.append(meaning['meaning'])
 
-    self.aux_meanings = []
     for aux_meaning in data['auxiliary_meanings']:
-      self.aux_meanings.append(meaning['meaning'])
+      self.aux_meanings.append(aux_meaning['meaning'])
+    if len(self.aux_meanings) == 0:
+      self.aux_meanings.append('None')
 
-    self.meaning_mnemonic = data['meaning_mnemonic']
+  def to_anki(self):
+    """
+    @return (str) a tab-separated list of our members.
+    """
+    # Some of our members are lists that will be joined by commas. Anki uses
+    # the first (comma, semicolon, tab) it sees as the separator. Therefore,
+    # the first member we add to the string should not itself contain any
+    # character that can be confused as a separator.
+    return str(self.ANKI_SEPARATOR.join((str(self.id), self.document_url,
+                                         str(self.level),
+                                         ', '.join(self.meanings),
+                                         ', '.join(self.aux_meanings),
+                                         self.meaning_mnemonic)))
+
+  @staticmethod
+  def anki_schema():
+    """
+    @return (str) Our members in order of appearance in to_anki().
+    """
+    return "id document_url level meanings aux_meanings meaning_mnemonic"
 
 
 class Radical(Subject):
@@ -45,9 +75,25 @@ class Radical(Subject):
       the WaniKani V2 API.
     @p store_json (bool) If true, store @p item in this instance.
     """
-    super().__init__(item, store_json)
+    # The definition of members is important. We take the order of definition
+    # to be the order in which our members are written out in the Anki schema.
 
+    super().__init__(item, store_json)
     self.characters = ''  # TODO(orphen) Store the radical's vector graphic.
+
+  def to_anki(self):
+    """
+    @return (str) a tab-separated list of our members.
+    """
+    anki = super().to_anki()
+    return self.ANKI_SEPARATOR.join((anki, 'characters_not_implemented'))
+
+  @staticmethod
+  def anki_schema():
+    """
+    @return (str) Our members in order of appearance in to_anki().
+    """
+    return ' '.join((Subject.anki_schema(), 'characters'))
 
   def __str__(self):
     return ('Radical; Meanings: {}; Level: {}; ID: {}'
@@ -72,11 +118,17 @@ class Kanji(Subject):
       the WaniKani V2 API.
     @p store_json (bool) If true, store @p item in this instance.
     """
+    # The definition of members is important. We take the order of definition
+    # to be the order in which our members are written out in the Anki schema.
+
     super().__init__(item, store_json)
+
     data = item['data']
 
     self.characters = data['characters']
     self.readings = self.Readings()
+    self.reading_mnemonic = data['reading_mnemonic']
+
     for reading in data['readings']:
       if reading['type'] == 'onyomi':
         self.readings.onyomi.append(reading['reading'])
@@ -85,7 +137,22 @@ class Kanji(Subject):
       if reading['type'] == 'nanori':
         self.readings.nanori.append(reading['reading'])
 
-    self.reading_mnemonic = data['reading_mnemonic']
+
+  def to_anki(self):
+    """
+    @return (str) a tab-separated list of our members.
+    """
+    anki = super().to_anki()
+    # TODO(orphen)
+    return anki
+
+  @staticmethod
+  def anki_schema():
+    """
+    @return (str) Our members in order of appearance in to_anki().
+    """
+    return ' '.join((Subject.anki_schema(), 'characters', 'readings',
+                     'reading_mnemonic'))
 
   def __str__(self):
     return ('Kanji; Character: {}; Meanings: {}; Readings: {}; Level: {}; '
@@ -103,24 +170,43 @@ class Vocabulary(Subject):
       through the WaniKani V2 API.
     @p store_json (bool) If true, store @p item in this instance.
     """
+    # The definition of members is important. We take the order of definition
+    # to be the order in which our members are written out in the Anki schema.
+
     super().__init__(item, store_json)
+
     data = item['data']
 
     self.characters = data['characters']
-
     self.readings = list()
+    self.parts_of_speech = list()
+    self.reading_mnemonic = data['reading_mnemonic']
+    self.sentences = list()
+
     for reading in data['readings']:
       self.readings.append(reading['reading'])
 
-    self.parts_of_speech = list()
     for part in data['parts_of_speech']:
       self.parts_of_speech.append(part)
 
-    self.reading_mnemonic = data['reading_mnemonic']
-
-    self.sentences = list()
     for sentence in data['context_sentences']:
       self.sentences.append(self.Sentence(sentence['en'], sentence['ja']))
+
+  def to_anki(self):
+    """
+    @return (str) a tab-separated list of our members.
+    """
+    anki = super().to_anki()
+    # TODO(orphen)
+    return anki
+
+  @staticmethod
+  def anki_schema():
+    """
+    @return (str) Our members in order of appearance in to_anki().
+    """
+    return ' '.join((Subject.anki_schema(), 'characters', 'readings',
+                     'parts_of_speech', 'reading_mnemonic', 'sentences'))
 
   def __str__(self):
     return ('Vocabulary; Characters: {}; Meanings: {}; Readings: {}; '
