9684697040495c736c5a4d9290b0ae8ff6555a12
orphen <orphen@orphen.me>
2019-10-31 18:46:01 -0700

    lesson_reorder: refactored dynamic HTML into strings
    This also adds a basic form for configuration options. Form
    selections are not handled yet.

--- tampermonkey/lesson_reorder.js
+++ tampermonkey/lesson_reorder.js
@@ -16,6 +16,11 @@
   // type, leaving the user to work through all *reviews* in any order.
 
   const ReorderModalCSS = `
+    <style>
+    #wktk-reorder-li {
+      cursor: pointer;
+    }
+
     .wktk-reorder-modal {
       display: none;
       position: fixed;
@@ -41,45 +46,91 @@
       float: right;
       cursor: pointer;
     }
+
+    .wktk-row:after {
+      content: "";
+      display: table;
+      clear: both;
+    }
+
+    .wktk-col-25 {
+      float: left;
+      width: 15%;
+      margin-top: 5px;
+    }
+
+    .wktk-col-75 {
+      float: left;
+      width: 85%;
+      margin-top: 5px;
+    }
+
+    .wktk-col-75 span {
+      margin-right: 10px;
+    }
+    </style>
   `;
+
+    const ReorderControlHTML = `<li id='wktk-reorder-li'>[Reorder]</li>`;
+
+    const ReorderModalHTML = `
+      <div id='wktk-reorder-modal' class='wktk-reorder-modal'>
+        <div class='wktk-reorder-modal-content'>
+          <h1>Reorder Configuration</h1>
+          <form id='wktk-reorder-configuration-form'>
+            <div class='wktk-row'>
+              <div class='wktk-col-25'>Enabled</div>
+              <div class='wktk-col-75'>
+                <input type='checkbox' name='wktk-enabled' value='enabled' checked>
+              </div>
+            </div>
+            <br>
+            <div class='wktk-row'>
+              <div class='wktk-col-25'><b>Order</b></div>
+              <div class='wktk-col-75'><b>Subject</b></div>
+            </div>
+            <div class='wktk-row'>
+              <div class='wktk-col-25'>First</div>
+              <div class='wktk-col-75'>
+                <span><input type='radio' name='wktk-first' value='radical'> Radical</span>
+                <span><input type='radio' name='wktk-first' value='kanji'> Kanji</span>
+                <span><input type='radio' name='wktk-first' value='vocabulary'> Vocabulary</span>
+              </div>
+            </div>
+            <div class='wktk-row'>
+              <div class='wktk-col-25'>Then</div>
+              <div class='wktk-col-75'>
+                <span><input type='radio' name='wktk-second' value='radical'> Radical</span>
+                <span><input type='radio' name='wktk-second' value='kanji'> Kanji</span>
+                <span><input type='radio' name='wktk-second' value='vocabulary'> Vocabulary</span>
+              </div>
+            </div>
+            <div class='wktk-row'>
+              <div class='wktk-col-25'>Finally</div>
+              <div class='wktk-col-75'>?</div>
+            </div>
+          </form>
+          <span id='wktk-reorder-modal-close' class='wktk-reorder-modal-close'>[close]</span>
+        </div>
+      </div>
+    `;
 
   /**
    * Prepends a Reorder indicator to the WaniKani stats list in lessons. This indicator can be clicked to open the
    * reordering configuration. Also creates the initially-hidden reordering configuration modal.
-   * @return The reorder element (HTMLLIElement) added to the stats list.
    */
   const AddReorderElements = () => {
-    // The reordering configuration modal.
-    document.head.insertAdjacentHTML('beforeend', `<style>${ReorderModalCSS}</style>`);
+    document.getElementById('stats').querySelector('ul').insertAdjacentHTML('afterbegin', ReorderControlHTML);
+    document.head.insertAdjacentHTML('beforeend', ReorderModalCSS);
+    document.body.insertAdjacentHTML('afterbegin', ReorderModalHTML);
 
-    let reorder_modal = document.createElement('div');
-    reorder_modal.id = 'wktk-reorder-modal';
-    reorder_modal.classList.add('wktk-reorder-modal');
+    // Modal element event handlers.
+    const ModalElement = document.getElementById('wktk-reorder-modal');
+    document.getElementById('wktk-reorder-modal-close').onclick = event => ModalElement.style.display = 'none';
 
-    let reorder_modal_content = document.createElement('div');
-    reorder_modal_content.classList.add('wktk-reorder-modal-content');
-    reorder_modal_content.innerHTML = 'TODO';
-
-    let reorder_modal_close = document.createElement('span');
-    reorder_modal_close.classList.add('wktk-reorder-modal-close');
-    reorder_modal_close.innerHTML = '[close]';
-    reorder_modal_close.onclick = event => reorder_modal.style.display = 'none';
-
-    reorder_modal_content.append(reorder_modal_close);
-    reorder_modal.append(reorder_modal_content);
-    document.body.prepend(reorder_modal);
-
-    // The visible Reorder control.
-    let reorder_li = document.createElement('li');
-    reorder_li.id = 'wktk-reorder-li';
-    reorder_li.style.cursor = 'pointer';
-    reorder_li.innerHTML = '[Reorder]';
-    reorder_li.onclick = event => reorder_modal.style.display = 'block';
-
-    document.getElementById('stats').querySelector('ul').prepend(reorder_li);
-
-    return reorder_li;
+    // Visible Reorder control event handlers.
+    document.getElementById('wktk-reorder-li').onclick = event => ModalElement.style.display = 'block';
   };
 
-  const reorder_element = AddReorderElements();
+  AddReorderElements();
 })();
